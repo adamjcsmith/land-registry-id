d48af475e2b343dff31ffc31ef1800d24c72b9a2
Adam Smith <adam@settled.co.uk>
2018-11-07 12:01:42 +0000

    Added routes for login, agent and seller

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-const Home = () => (
+const Login = () => (
   <div>
     This will be the home page!
   </div>
 );
 
+const Agent = () => (
+  <div>
+    Agent form will be here.
+  </div>
+);
+
+const Seller = () => (
+  <div>
+    Seller form will be here.
+  </div>
+);
 
 class App extends Component {
   render() {
@@ -17,10 +28,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <div>
-            <Route exact path="/" component={Home} />
-            {/* <Route exact path="/login" component={Login} />
-                <Route exact path="/agent" component={Agent} />
-                <Route exact path="/seller" component={Seller} /> */}
+            <Route exact path="/" component={Login} />
+            <Route exact path="/agent" component={Agent} />
+            <Route exact path="/seller" component={Seller} />
           </div>
         </div>
       </Router>
