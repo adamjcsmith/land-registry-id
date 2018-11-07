13176413104ec306cea297030579de5fccd8adc5
Adam Smith <adam@settled.co.uk>
2018-11-07 11:59:40 +0000

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+const Home = () => (
+  <div>
+    This will be the home page!
+  </div>
+);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/login" component={Login} />
+                <Route exact path="/agent" component={Agent} />
+                <Route exact path="/seller" component={Seller} /> */}
+          </div>
+        </div>
+      </Router>
     );
   }
 }
